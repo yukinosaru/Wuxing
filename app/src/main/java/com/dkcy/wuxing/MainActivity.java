f4204623f01f26924fac056b3c8c2da9608ae15b
Daniel Yeo <dan@dkcy.com>
2016-12-16 09:30:56 +0000

    Working
    Added action bar and date picker

--- app/src/main/java/com/dkcy/wuxing/MainActivity.java
+++ app/src/main/java/com/dkcy/wuxing/MainActivity.java
@@ -1,12 +1,27 @@
 package com.dkcy.wuxing;
 
+import android.annotation.TargetApi;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
 
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Sets toolbar as the app bar for the activity
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
         // Set up the database helper for accessing data
         try {
@@ -33,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         myHelper.close();
 
-        /* Set up the date picker */
+        /* Set up the date picker - original approach of a visible DatePicker as card.
         DatePicker datePicker = (DatePicker) findViewById(R.id.date_of_birth);
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
@@ -50,18 +69,82 @@ public class MainActivity extends AppCompatActivity {
                 myHelper.close();
             }
         });
+*/
+
+        /* Experimenting with input toolbar
+        // Set up the input toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //Intent myIntent = new Intent(view.getContext(), agones.class);
+                //startActivityForResult(myIntent, 0);
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create(); //Read Update
+                alertDialog.setTitle("hi");
+                alertDialog.setMessage("this is my app");
+
+                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // here you can add functions
+                    }
+                });
+
+                alertDialog.show();  //<-- See This!
+            }
+        });*/
 
     }
 
+    // Set up actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        // Only has one item at the mo...
+        int id = item.getItemId();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        int now_year = calendar.get(Calendar.YEAR);
+        int now_month = calendar.get(Calendar.MONTH);
+        int now_dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // Creates DatePickerDialog (simpler method in API24...)
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,R.style.AppTheme,new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                myHelper.openDataBase();
+                String[] sign = myHelper.lookupDateOfBirth(year, month, dayOfMonth);
+                zodiac.setZodiac(sign);
+                displayZodiac();
+                myHelper.close();
+            }
+        },now_year,now_month,now_dayOfMonth);
+
+        datePickerDialog.setTitle("Enter date of birth");
+
+        datePickerDialog.show();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
     public void displayZodiac(){
         String[] sign = zodiac.getZodiac();
         String outcome = "You're a " + sign[0] + " " + sign[3];
         final TextView textZodiac = (TextView) findViewById(R.id.zodiac_sign);
+        textZodiac.setText(outcome);
+
         final TextView textAnimal = (TextView) findViewById(R.id.animal);
         final TextView textElement = (TextView) findViewById(R.id.element);
-        textZodiac.setText(outcome);
+
         textAnimal.setText(sign[5]);
         textElement.setText(sign[2]);
     }
-
 }
